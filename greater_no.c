5fe436dd8b115236e12c434cb68bae79048e06b5
payaldhok18 <140889112+payaldhok18@users.noreply.github.com>
2023-11-08 00:58:28 +0530

    Create greater_no.c

---
+++ greater_no.c
@@ -0,0 +1,17 @@
+/*#include <stdio.h>
+int main()
+{   
+   int a,b;
+   printf("enter 1st number\n");
+   scanf("%d",&a);
+   
+   printf("enter 2nd number\n");
+   scanf("%d",&b);
+   
+   if(a>b)
+   {printf("greater is %d",a);}
+   
+   else
+   {printf("greater is %d",b);}
+   return 0;
+}
