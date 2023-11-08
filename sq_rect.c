ce4ac536903690309ac2003f53bde465db23d9da
payaldhok18 <140889112+payaldhok18@users.noreply.github.com>
2023-11-08 11:28:21 +0530

    Create sq_rect.c

---
+++ sq_rect.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{   
+    int s1,s2;
+    printf("enter side one\n");
+    scanf("%d",&s1);
+    
+    printf("enter side two\n");
+    scanf("%d",&s2);
+    
+    if (s1==s2)
+        {printf("it is a square ");}
+      
+    else 
+       {printf("it is rectangle");}
+}
