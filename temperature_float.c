dc941776bd994feb7dd9233cf2baada4815da154
payaldhok18 <140889112+payaldhok18@users.noreply.github.com>
2023-11-08 11:37:45 +0530

    Create temperature_float.c

---
+++ temperature_float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>// temperature in float value
+int main()
+{
+ int lower;
+ for(lower=0; lower<=300; lower=lower+20)
+ {  
+ printf("%d\t%f\n", lower, 5.0*(lower-32.0)/9.0);
+ }
+ return 0;
+}*/
